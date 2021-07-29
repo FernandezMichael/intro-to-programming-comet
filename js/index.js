cda70be8f24398df83532c46c1ede73b7ca87eba
Michael Fernandez <mikhail.fernandez@gmail.com>
2021-07-29 01:42:32 -0400

    Lesson 4.3 with optional stretch goal #1

--- js/index.js
+++ js/index.js
@@ -12,3 +12,42 @@ for (let i=0; i<skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.getElementById('leave_message');
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nameInput = document.getElementById("name")
+    console.log(nameInput.value);
+    const emailInput = document.getElementById("email");
+    console.log(emailInput.value);
+    const messageInput = document.getElementById("message");
+    console.log(messageInput.value);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href=\"mailto:${emailInput.value}\">${nameInput.value}</a>`+
+        `<span> wrote: ${messageInput.value} </span>`;
+    console.log(newMessage.innerHTML);
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', (e) => {
+        const entry = removeButton.parentElement;
+        entry.remove();
+        // optional stretch goal 1
+        if (!messageList.children.length) messageSection.style.display = 'none';
+    })
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageSection.style.display = 'block'; // optional stretch goal 1
+
+    // reset
+    nameInput.value='';
+    emailInput.value='';
+    messageInput.value='';         
+    
+})
